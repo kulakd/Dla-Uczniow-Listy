93e959d82664844e03fa31ce32f2a16be813b91f
Dawid <dkulakowski1999@gmail.com>
2024-10-14 12:33:35 +0200

    Add project files.

---
+++ Pilkarz.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListyiFE
+{
+    public class Pilkarz
+    {
+        // Pola
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Pozycja { get; set; }
+        public int Numer { get; set; }
+
+        // Konstruktor
+        public Pilkarz(string imie,string nazwisko, string pozycja, int numer)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Pozycja = pozycja;
+            Numer = numer;
+        }
+
+        // Opis piłkarza
+        public void Opis()
+        {
+            Console.WriteLine($"Piłkarz: {Imie} {Nazwisko}, Pozycja: {Pozycja}, Numer: {Numer}");
+        }
+
+        //Sprawdzenie, czy piłkarz gra na bramce, ToLower() zamienia wszytskie litery na małe
+        public bool CzyJestBramkarzem()
+        {
+            return Pozycja.ToLower() == "bramkarz";
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿namespace ListyiFE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // porównanie Tabli i List
+            //Tablica();
+            //Console.WriteLine();
+            //Listy();
+
+            // Tworzenie listy piłkarzy
+            List<Pilkarz> pilkarze = new List<Pilkarz>();
+            Pilkarz P1 = new Pilkarz("Robert", " Lewandowski", "Napastnik", 9);
+            Pilkarz P2 = new Pilkarz("Wojciech", " Szczęsny", "Bramkarz", 1);
+            Pilkarz P3 = new Pilkarz("Kamil", "Glik", "Obrońca", 15);
+
+            // Dodanie Piłkarzy do listy
+            pilkarze.Add(P1);
+            pilkarze.Add(P2);
+            pilkarze.Add(P3);
+
+
+            // Iteracja po piłkarzach za pomocą foreach
+            // foreach ( TYP ZMIENNA in LISTA), czytamy "dla każdego p klasy Piłkarz w liscie pilkarze"
+            foreach (Pilkarz p in pilkarze)
+            {
+                p.Opis(); // Wywołanie metody Opis
+                if (p.CzyJestBramkarzem()) // Sprawdzanie, czy piłkarz jest bramkarzem
+                {
+                    Console.WriteLine($"{p.Imie} jest bramkarzem.");
+                }
+                else
+                {
+                    Console.WriteLine($"{p.Imie} nie jest bramkarzem.");
+                }
+                Console.WriteLine();
+            }
+
+        }
+        // Porównujemy działanie tablic i list
+        #region Tablica
+        public static void Tablica()
+        {
+            int[] liczby = new int[5];
+            double suma = 0;
+            for (int i = 0; i < liczby.Length; i++)
+            {
+                Console.WriteLine("Podaj nową liczbę: ");
+                int liczba = int.Parse(Console.ReadLine());
+                liczby[i] = liczba;
+                suma += liczba;
+
+                string tekst = ZamienNaTekst(liczby);
+                int liczbaElementow = i + 1;
+                double srednia = suma / liczbaElementow;
+                Console.WriteLine($"Aktualna tablica: {tekst}");
+                Console.WriteLine($"Jest {liczbaElementow} liczb, średnia to {srednia}");
+            }
+            Console.ReadKey();
+        }
+        public static string ZamienNaTekst(int[] tablica)
+        {
+            string wynik = "[";
+            for (int i = 0; i < tablica.Length; i++)
+                wynik += $"{tablica[i]}";
+            wynik += "]";
+            return wynik;
+        }
+        #endregion
+
+        #region Listy
+        public static void Listy()
+        {
+            List<int> list = new List<int>();
+            double suma = 0;
+            while (true)
+            {
+                Console.WriteLine("Podaj nową liczbę: ");
+                int liczba = int.Parse(Console.ReadLine());
+                list.Add(liczba);
+                suma += liczba;
+                string tekst = ZamienNaTekst(list);
+                int liczbaElementow = list.Count;
+                double srednia = suma / list.Count;
+                Console.WriteLine($"Aktualna lista: {tekst}");
+                Console.WriteLine($"Jest {liczbaElementow} liczb, średnia to {srednia}");
+            }
+        }
+        public static string ZamienNaTekst(List<int> lista)
+        {
+            string wynik = "[";
+            for (int i = 0; i < lista.Count; i++)
+                wynik += $"{lista[i]}";
+            wynik += "]";
+            return wynik;
+        }
+
+        #endregion
+    }
+}
